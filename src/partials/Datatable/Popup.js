6ea59cd8beac0024f839b6b74e49a48250028bbe
Ahmed Wanneni <ahmed.wanneni@gmail.com>
2021-10-21 15:36:16 +0100

    21/10/2021

--- src/partials/Datatable/Popup.js
+++ src/partials/Datatable/Popup.js
@@ -1,55 +1,66 @@
-import React from 'react'
-import {Dialog , DialogTitle, DialogContent, Typography,Button} from '@material-ui/core';
+import React from "react";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  Typography,
+  Button,
+} from "@material-ui/core";
 //import CloseIcon from '@mui/icons-material/Close';
-import { faTimes} from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { useStyles } from "./style";
 function Popup(props) {
   const classes = useStyles();
-    const {title, children, openPopup, setOpenPopup} = props;
+  const { title, children, openPopup, setOpenPopup } = props;
 
-    return (<>
-        <Dialog 
-        fullScreen 
-        
-        open={openPopup}>
-             <header className="sticky top-0 border-b border-gray-200 z-30 header">
-      <div className="px-4 sm:px-6 lg:px-8">
-     
-        <div className="flex items-center justify-between h-16 -mb-px">
-        <div className="flex justify-content-center items-center"></div>
-          {/* Header: Left side */}
-          <div className="flex">
-          <Typography component="h1" variant="h4" align="center" style={{color: "#000000"}}>
-                    {title}
+  return (
+    <>
+      <Dialog fullScreen open={openPopup}>
+        <header className="sticky top-0 border-b border-gray-200 z-30 header">
+          <div className="px-4 sm:px-6 lg:px-8">
+            <div className="flex items-center justify-between h-16 -mb-px">
+              <div className="flex justify-content-center items-center"></div>
+              {/* Header: Left side */}
+              <div className="flex">
+                <Typography
+                  component="h1"
+                  variant="h4"
+                  align="center"
+                  style={{ color: "#000000" }}
+                >
+                  {title}
                 </Typography>
+              </div>
+
+              {/* Header: Right side */}
+              <div
+                className="flex"
+                style={{ alignItems: "center", justifyContent: "center" }}
+              >
+                <FontAwesomeIcon
+                  onClick={() => {
+                    setOpenPopup(false);
+                  }}
+                  icon={faTimes}
+                  style={{ width: "32px", height: "32px", color: "black" }}
+                />
+              </div>
+            </div>
           </div>
+        </header>
 
-          {/* Header: Right side */}
-          <div className="flex" style={{alignItems:"center",justifyContent:"center"}}>
-        
-                <FontAwesomeIcon  
-          onClick={() => {
-            setOpenPopup(false)
-          }}
-          icon={faTimes}  style={{width:"32px",height:"32px",color:"black"}} />
-
-          </div>
-
-        </div>
-      </div>
-    </header>
-           
-            <DialogContent className={classes.dc} dividers
-            //  style={{alignItems:"center",alignSelf:"center",maxWidth:'100vh',marginTop:"10vh"}}
-             >
-                {children}
-            </DialogContent>
-            
-        </Dialog>
-        </>
-    )
+        <DialogContent
+          className={classes.dc}
+          dividers
+          //  style={{alignItems:"center",alignSelf:"center",maxWidth:'100vh',marginTop:"10vh"}}
+        >
+          {children}
+        </DialogContent>
+      </Dialog>
+    </>
+  );
 }
 
-export default Popup
+export default Popup;
